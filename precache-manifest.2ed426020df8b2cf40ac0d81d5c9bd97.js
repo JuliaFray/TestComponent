6479b63f19ba41168f486420ac07d0945747562f
JuliaFray <julia.pticami@gmail.com>
2020-08-21 20:26:36 +0700

    Updates

---
+++ precache-manifest.2ed426020df8b2cf40ac0d81d5c9bd97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ef32e440b8d0479121e3e80f34a76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d369d5dba13e657c7565",
+    "url": "/static/css/main.e44dec1c.chunk.css"
+  },
+  {
+    "revision": "6b180536a3b7fbaf3c7a",
+    "url": "/static/js/2.50125a09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50125a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d369d5dba13e657c7565",
+    "url": "/static/js/main.7415cdbc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
